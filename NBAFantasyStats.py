db6df71abfb333a17f00451ac72398ab29341b8a
Milos Bijelic <mbijelic.03@gmail.com>
2021-01-27 21:58:21 -0500

    renamed variables to be more intuitive

--- NBAFantasyStats.py
+++ NBAFantasyStats.py
@@ -3,7 +3,7 @@ import json
 from json import dumps
 import os
 import time
-import pdb
+
 
 class YahooNBAF:
 	def __init__(self): 
@@ -15,7 +15,7 @@ class YahooNBAF:
 
 
 	# gets response from Yahoo Fantasy API as a JSON object
-	def yahooAPI(self, url): 
+	def getResponse(self, url): 
 		oauth = OAuth2(None, None, from_file='./authorization/oauth2yahoo.json')
 		if not oauth.token_is_valid():
 		    oauth.refresh_access_token()
@@ -27,27 +27,27 @@ class YahooNBAF:
 
 	def getGameKey(self): 
 		general_info = f'https://fantasysports.yahooapis.com/fantasy/v2/game/nba'
-		general_response = self.yahooAPI(general_info)
+		general_response = self.getResponse(general_info)
 		game_key = general_response['fantasy_content']['game'][0]['game_key']
 		return game_key
 
 
 	def getCurrentWeek(self):
 		week_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+ self.game_key+'.l.'+self.league_id
-		current_week = self.yahooAPI(week_url)['fantasy_content']['league'][0]['current_week']
+		current_week = self.getResponse(week_url)['fantasy_content']['league'][0]['current_week']
 		return current_week
 
 
 	def getNumTeams(self):
 		league_info_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+ self.game_key+'.l.'+self.league_id
-		league_info = self.yahooAPI(league_info_url)
+		league_info = self.getResponse(league_info_url)
 		num_teams = league_info['fantasy_content']['league'][0]['num_teams']
 		return num_teams
 
 
 	def getMaxPlayers(self): 
 		players_url = 'https://fantasysports.yahooapis.com/fantasy/v2/team/'+self.game_key+'.l.'+self.league_id+'.t.'+str(self.team_num)+'/roster/players'
-		players_count = self.yahooAPI(players_url)
+		players_count = self.getResponse(players_url)
 		max_players = players_count['fantasy_content']['team'][1]['roster']['0']['players']['count']
 		return max_players
 
@@ -57,7 +57,7 @@ class YahooNBAF:
 	def createStatsLUT(self):
 		statsLUT = {}
 		league_settings_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id+'/settings'
-		response = self.yahooAPI(league_settings_url)
+		response = self.getResponse(league_settings_url)
 		stats_list = response['fantasy_content']['league'][1]['settings'][0]['stat_categories']['stats']
 		num_stats = len(stats_list)
 		for i in range(0, num_stats):
@@ -92,7 +92,7 @@ class YahooNBAF:
 			player_name = roster['fantasy_content']['team'][1]['roster']['0']['players'][str(player)]['player'][0][2]['name']['full']
 
 			player_stats_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id+'/players;player_keys='+self.game_key+'.p.'+player_id+'/stats'
-			player_stats = self.yahooAPI(player_stats_url) 
+			player_stats = self.getResponse(player_stats_url) 
 			
 			stats['players'].append({"player_name": player_name, "player_id": player_id, "player_stats": player_stats['fantasy_content']['league'][1]['players']['0']['player'][1]['player_stats']['stats']}) 
 			
@@ -105,9 +105,9 @@ class YahooNBAF:
 
 	def getTeamsWeeklyStats(self):
 		for week in range(1, self.current_week+1):
-			for matchup in range(6): # 0-5 (6 matchups, 12 teams)
+			for matchup in range(int(self.num_teams/2)): # 0-5 (6 matchups, 12 teams)
 					url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id+'/scoreboard;week='+str(week)
-					stats = self.yahooAPI(url)
+					stats = self.getResponse(url)
 
 					for team in range(2): # H2H - 2 teams 0 and 1
 						team_name = stats['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][str(matchup)]['matchup']['0']['teams'][str(team)]['team'][0][2]['name']
@@ -126,7 +126,7 @@ class YahooNBAF:
 		# pulls each teams roster info and saves it to: "./rosters"
 		for team in range(1, self.num_teams + 1):
 			roster_url = 'https://fantasysports.yahooapis.com/fantasy/v2/team/'+self.game_key+'.l.'+self.league_id+'.t.'+str(team)+'/roster;week='+str(self.current_week)
-			roster = self.yahooAPI(roster_url) 
+			roster = self.getResponse(roster_url) 
 			team_name = roster['fantasy_content']['team'][0][2]['name']
 			file_name = str(team_name) + '_roster.json'
 			with open('./data/teams/'+team_name+'/'+file_name, 'w') as outfile:
