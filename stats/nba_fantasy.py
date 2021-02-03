384bfd616c95eb83576f713524f08a98388088e8
Brandon Rufino <brandonrufino@hotmail.ca>
2021-02-03 14:00:11 -0500

    draft players .csv complete.

--- stats/nba_fantasy.py
+++ stats/nba_fantasy.py
@@ -2,6 +2,8 @@
 import sys
 import pip
 import subprocess
+import os
+import time
 
 # installs all non-core python libraries required
 # --------------------------------------------------------------------------------------------------   
@@ -30,6 +32,7 @@ if missing:
 from yahoo_oauth import OAuth2
 import yahoo_fantasy_api as yfa
 import json
+import pandas as pd
 
 # authorization path - see readme for more details.
 AUTH_PATH = '../authorization/authorization_info.json'
@@ -37,7 +40,7 @@ AUTH_PATH = '../authorization/authorization_info.json'
 class YahooNBAF:
     def __init__(self): 
         # get session
-        self.session = OAuth2(None, None, from_file='../authorization/authorization_info.json')
+        self.session = OAuth2(None, None, from_file=AUTH_PATH)
         
         # get game info
         self.__game = self.__setGame()
@@ -96,7 +99,109 @@ class YahooNBAF:
     def getStatCategories(self):
         return self.__league.stat_categories()
     
-    # file dumps
+    def getPlayerOverallStats(self, plyr, stat_map):
+        """
+        gets current player stats
+            input:
+                self - YAHOONBAF object
+                plyr - player details for current season
+            output:
+                ply_stats - dict of stats for current season ['FG%','FT%','3PT','PTS','REB','ASSITS','STEALS','BLOCKS','TOS','FGM_A','FTM_A']
+        """
+        # get player stats
+        stat_id_map = stat_map
+        stats = {stat_id_map[stat['stat']['stat_id']]: stat['stat']['value'] for stat in plyr[0]['player_stats']['stats']}  
+        return stats
+    
+    def getImpactFG(self,df):
+        """
+        gets impact fg. impact fg defined as:
+        p-P=d
+        d*a=m
+        where:
+            p = player average
+            P = league average
+            d = difference of player average against league average
+            a = player attempts
+            m = impact score
+        input:
+            df - with player name and category stats
+        output:
+            df_impact_fg - df with included impact scores
+        """        
+        P = df['FG%'].mean()
+            
+        def attempts(x):
+            a = int(x['FGM/A'].split('/')[0])
+            return a
+        
+        df['IMPACT_FG'] = df.apply(lambda x: ((x['FG%']-P)*attempts(x)),axis=1)
+        return df
+    
+    def getImpactFT(self,df):
+        """
+        gets impact ft. impact ft defined as:
+        p-P=d
+        d*a=m
+        where:
+            p = player average
+            P = league average
+            d = difference of player average against league average
+            a = player attempts
+            m = impact score
+        input:
+            df - with player name and category stats
+        output:
+            df_impact_ft - df with included impact scores
+        """        
+        P = df['FT%'].mean()
+            
+        def attempts(x):
+            a = int(x['FTM/A'].split('/')[0])
+            return a
+        
+        df['IMPACT_FT'] = df.apply(lambda x: ((x['FT%']-P)*attempts(x)),axis=1)
+        return df
+
+    # helper functions
+    def createStatsLUT(self):
+        """
+        define a look up table for the stat names. i.e. translate ID(9007006) to ('FG%')
+        """
+        stat_list=self.__league.yhandler.get_settings_raw(self.__league_id)['fantasy_content']['league'][1]['settings'][0]['stat_categories']['stats']
+        statsLUT = {str(stat['stat']['stat_id']): stat['stat']['display_name'] for stat in stat_list}
+        return statsLUT
+    
+    def replaceWithZero(self, df):
+        """
+        replaces empty stats with zero. convert stats to proper data type.
+        """ 
+        df['FGM/A'] = df['FGM/A'].replace('-/-', '0/0')
+        df['FGM/A'] = df['FGM/A'].astype(str)
+        df['FG%'] = df['FG%'].replace('-', '0')
+        df['FG%'] = df['FG%'].astype(float)
+        df['FTM/A'] = df['FTM/A'].replace('-/-', '0/0')
+        df['FTM/A'] = df['FTM/A'].astype(str)
+        df['FT%'] = df['FT%'].replace('-', '0')
+        df['FT%'] = df['FT%'].astype(float)
+        df['3PTM'] = df['3PTM'].replace('-', '0')
+        df['3PTM'] = df['3PTM'].astype(int)
+        df['PTS'] = df['PTS'].replace('-', '0')
+        df['PTS'] = df['PTS'].astype(int)
+        df['REB'] = df['REB'].replace('-', '0')
+        df['REB'] = df['REB'].astype(int)
+        df['AST'] = df['AST'].replace('-', '0')
+        df['AST'] = df['AST'].astype(int)
+        df['ST'] = df['ST'].replace('-', '0')
+        df['ST'] = df['ST'].astype(int)
+        df['BLK'] = df['BLK'].replace('-', '0')
+        df['BLK'] = df['BLK'].astype(int)
+        df['TO'] = df['TO'].replace('-', '0')
+        df['TO'] = df['TO'].astype(int)
+        return df
+        
+    
+    # file dump functions
     """
         TODO:
             create csv file
@@ -106,12 +211,18 @@ class YahooNBAF:
         # create team map
         teams = self.__league.teams()
         team_map = {teams[e]['team_key']: teams[e]['name'] for e in teams}
+        stat_map = self.createStatsLUT()
+        num_teams = self.getNumTeams()
         # get draft results
         draft_results = self.__league.draft_results()
         print("{} draft picks selected".format(len(draft_results)))
         ids = [e['player_id'] for e in draft_results]
         self.__league.player_details(ids)   # Prime the player detail cache
-#        print(self.__league.player_details(ids[0]))
+        
+        # create dataframe
+        stat_columns = [sc['display_name'] for sc in self.getStatCategories()]
+        cols = ['pick', 'round', 'team', 'player', 'position'] + stat_columns + ['FGM/A', 'FTM/A']
+        output = pd.DataFrame(columns=cols)   
         for dp in draft_results:
             plyr = self.__league.player_details(dp['player_id'])
             if "cost" in dp:
@@ -121,17 +232,56 @@ class YahooNBAF:
                 pick_and_cost = "Pick: {:4} ".format(dp['pick'])
             player_and_team = "Player: {:30} Team: {:30}".format(
                 plyr[0]['name']['full'], team_map[dp['team_key']])
-            print(pick_and_cost + player_and_team)        
+            
+            # get draft info
+            basic_info = {}
+            basic_info['pick'] = dp['pick']
+            basic_info['round'] = ((dp['pick']-1)//num_teams) + 1
+            basic_info['team'] = team_map[dp['team_key']]
+            basic_info['player'] = plyr[0]['name']['full']
+            basic_info['position'] = plyr[0]['primary_position']
+            
+            # get stats of player
+            stats = self.getPlayerOverallStats(plyr,stat_map)
+
+            draft_pick_summary = {**basic_info, **stats} # merge dicts
+
+            # append draft result to df
+            output = output.append(draft_pick_summary, ignore_index=True)
+            
+            print(pick_and_cost + player_and_team)
+        
+        # create impact columns
+        output = self.replaceWithZero(output) # convert empty stats to 0
+        output = self.getImpactFG(output)
+        output = self.getImpactFT(output)
+        
+        # export to .csv
+        outname = 'draft_results.csv'
+        
+        outdir = './fantasy_results/'
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        
+        fullname = os.path.join(outdir, outname)    
+        
+        output.to_csv(fullname,index=False) 
+        
         return
     
-    def teamResults(self):
+    """
+    CSV OUTPUT
+    """
+    def dumpPlayerOverallStats(self):
         return
     
-    def teamStats(self):
+    """
+    CSV OUTPUT
+    team | opponent | week | TCAT1 | OPPCAT1 | 
+    """
+    def dumpLeagueStats(self):
         return
     
-    def playerStats(self):
-        return
     
     
 # update output files for league
@@ -151,6 +301,7 @@ def updateFantasyLeague():
     ### generate files for data vis / analytics
     # draft results
     my_league.dumpDraftResults()
+    
     # team results
     
     # team stats
@@ -168,108 +319,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-#    def getMaxPlayers(self): 
-#        players_url = 'https://fantasysports.yahooapis.com/fantasy/v2/team/'+self.game_key+'.l.'+self.league_id+'.t.'+str(self.team_num)+'/roster/players'
-#        players_count = self.getResponse(players_url)
-#        max_players = players_count['fantasy_content']['team'][1]['roster']['0']['players']['count']
-#        return max_players
-#
-#
-#    # Defines a Look Up Table for the stat names. The Yahoo API returns stats with an id instead of a name - e.g. 'FG%' ID is 9007006
-#    # This LUT will be used to rename the stats from their ID (9007006) to their recognizable names (FG%)
-#    def createStatsLUT(self):
-#        statsLUT = {}
-#        league_settings_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id+'/settings'
-#        response = self.getResponse(league_settings_url)
-#        stats_list = response['fantasy_content']['league'][1]['settings'][0]['stat_categories']['stats']
-#        num_stats = len(stats_list)
-#        for i in range(num_stats):
-#            stat_id = stats_list[i]['stat']['stat_id']
-#            stat_name = stats_list[i]['stat']['display_name']
-#            statsLUT[stat_id] = stat_name
-#
-#        return statsLUT
-#
-#
-#    # Creates the json files inside data/team/team_name/team_weekly_stats/weekx 
-#    def createTeamWeeklyStatsJSON(self, data_obj, statsLUT): 
-#        stats = {}
-#        i = 0
-#        for entry in statsLUT.items(): 
-#            cat_name = str(entry[1])
-#            cat_val = str(data_obj[i]['stat']['value'])
-#            stats[cat_name] = cat_val
-#            i = i+1
-#        
-#        return stats
-#
-#
-#    def getLeagueInfo(self): 
-#        league_info_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id
-#        league_info = self.getResponse(league_info_url)
-#        with open('./data/league_info.json', 'w+') as outfile:
-#            json.dump(league_info, outfile)
-#
-#
-#    # pulls each rostered players total season stats and saves it to: "./rosters/roster_total_stats_2020"
-#    def getRosteredPlayersTotalStats(self, roster, roster_size): 
-#        team_name = roster['fantasy_content']['team'][0][2]['name']
-#        stats = {'team_name': str(team_name), 'players': []}
-#
-#        for player in range(roster_size):
-#            player_key = roster['fantasy_content']['team'][1]['roster']['0']['players'][str(player)]['player'][0][0]['player_key']
-#            player_id = roster['fantasy_content']['team'][1]['roster']['0']['players'][str(player)]['player'][0][1]['player_id']
-#            player_name = roster['fantasy_content']['team'][1]['roster']['0']['players'][str(player)]['player'][0][2]['name']['full']
-#
-#            player_stats_url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id+'/players;player_keys='+self.game_key+'.p.'+player_id+'/stats'
-#            player_stats = self.getResponse(player_stats_url) 
-#            
-#            stats['players'].append({"player_name": player_name, "player_id": player_id, "player_stats": player_stats['fantasy_content']['league'][1]['players']['0']['player'][1]['player_stats']['stats']}) 
-#            
-#            path = './data/teams/'+team_name+'/players_total_stats/'
-#            if not os.path.exists(path): 
-#                os.makedirs(path)
-#            with open('./data/teams/'+team_name+'/players_total_stats/'+'total_stats.json' , 'w+') as outfile:
-#                json.dump(stats, outfile)
-#
-#
-#    def getTeamsWeeklyStats(self):
-#        for week in range(1, self.current_week+1):
-#            for matchup in range(int(self.num_teams/2)): # 0-5 (6 matchups, 12 teams)
-#                    url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+self.game_key+'.l.'+self.league_id+'/scoreboard;week='+str(week)
-#                    stats = self.getResponse(url)
-#
-#                    for team in range(2): # H2H - 2 teams 0 and 1
-#                        team_name = stats['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][str(matchup)]['matchup']['0']['teams'][str(team)]['team'][0][2]['name']
-#                        path = './data/teams/'+team_name+'/team_weekly_stats/week'+str(week)
-#                        if not os.path.exists(path): 
-#                            os.makedirs(path)
-#                        stats_output = stats['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][str(matchup)]['matchup']['0']['teams'][str(team)]['team'][1]['team_stats']['stats']
-#                        weekly_stats = self.createTeamWeeklyStatsJSON(stats_output, self.statsLUT)
-#                        with open('./data/teams/'+team_name+'/team_weekly_stats/week'+str(week)+'/'+'team_stats.json', 'w+') as outfile:
-#                            json.dump(weekly_stats, outfile)
-#
-#
-#    # Generates all of the league data - roster info, weekly stats, etc. 
-#    def updateData(self): 
-#        self.getLeagueInfo()
-#        self.getTeamsWeeklyStats()
-#        
-#        # pulls each teams roster info and saves it to: "./rosters"
-#        for team in range(1, self.num_teams + 1):
-#            roster_url = 'https://fantasysports.yahooapis.com/fantasy/v2/team/'+self.game_key+'.l.'+self.league_id+'.t.'+str(team)+'/roster;week='+str(self.current_week)
-#            roster = self.getResponse(roster_url) 
-#            team_name = roster['fantasy_content']['team'][0][2]['name']
-#            file_name = str(team_name) + '_roster.json'
-#            with open('./data/teams/'+team_name+'/'+file_name, 'w') as outfile:
-#                json.dump(roster, outfile)
-#
-#            roster_size = roster['fantasy_content']['team'][1]['roster']['0']['players']['count']
-#
-#            # TODO: 
-#            # Commented out because overloads the API with calls and breaks wrapper
-#            # See how long of a wait needs to be added to not overload API with calls
-#
-#            self.getRosteredPlayersTotalStats(roster, roster_size)
